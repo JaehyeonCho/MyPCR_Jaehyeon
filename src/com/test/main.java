cc86f01e25f4d8f5a00a7d181785081b16bfeaa4
JaehyeonCho <jhcho1028@naver.com>
2015-07-13 15:44:12 +0900

    second commit

--- src/com/test/main.java
+++ src/com/test/main.java
@@ -1,10 +1,12 @@
 package com.test;
 
-public class main {
+public class main
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+		System.out.println("2015. 07. 13.");
+		System.out.println("MyPCR_Mentoring");
 	}
 
 }
